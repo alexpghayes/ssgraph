52537d8969dbbc8c410dca9207ea608045989355
alex hayes <alexpghayes@gmail.com>
2020-09-09 17:00:36 -0500

    asf

---
+++ scripts/04-find-venues-and-journals.R
@@ -0,0 +1,37 @@
+library(here)
+library(glue)
+library(jsonlite)
+library(dplyr)
+library(tidyr)
+
+##### parameters to change -----------------------------------------------------
+
+# also need to update clean_single_file signature
+
+version <- "2020-04-10"
+
+##### don't touch --------------------------------------------------------------
+
+json_paths <- list.files(
+  here(glue("data/{version}/json/")),
+  full.names = TRUE
+)
+
+handler <- function(df) {
+  df <- select(df, journalName, journalVolume)
+  df <- unnest(df, c(journalName, journalVolume))
+  df <- distinct(df)
+  stream_out(df)
+}
+
+for (path in json_paths) {
+
+  stream_in(
+    file(path),
+    handler = handler,
+    pagesize = 20000
+  )
+}
+
+# we're gonna go with Mathematics, Computer Science and Economics for now
+
