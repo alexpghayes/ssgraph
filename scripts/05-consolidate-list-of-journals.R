a52cb760fc114c04d946c9939788c07d6878d59c
alex hayes <alexpghayes@gmail.com>
2020-09-14 10:41:10 -0600

    Push code to cluster

---
+++ scripts/05-consolidate-list-of-journals.R
@@ -0,0 +1,30 @@
+library(here)
+library(glue)
+library(dplyr)
+library(tidyr)
+library(jsonlite)
+library(readr)
+library(furrr)
+plan(multisession, workers = 3)
+
+version <- "2020-04-10"
+
+paths <- list.files(
+  here(glue("output/{version}/journalName")),
+  pattern = "json",
+  full.names = TRUE
+)
+
+read_from_path <- function(path) {
+  df <- stream_in(file(path))
+  as_tibble(distinct(df, journalName))
+}
+
+all_journals <- future_map_dfr(paths, read_from_path)
+
+write_csv(
+  all_journals,
+  here("output/{version}/all_journals.csv")
+)
+
+plan(sequential)
